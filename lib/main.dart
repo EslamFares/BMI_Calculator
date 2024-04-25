2418e3182067b586c203c8316d72e301e8094ea2
EslamFares <eslamfareselazb@gmail.com>
2024-04-25 14:57:03 +0200

    Add Firebase authentication and core plugins Integrate Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:bmi/features/home/presention/views/home_view.dart';
+import 'package:bmi/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
